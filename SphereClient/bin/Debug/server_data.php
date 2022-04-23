71bbc5757aaf1ef0f41edc8df2c3aeefef523298
AlphMan <102704245+AlphMann@users.noreply.github.com>
2022-04-23 05:15:11 +0000

    Add files via upload

---
+++ SphereClient/bin/Debug/server_data.php
@@ -0,0 +1,11 @@
+server|127.0.0.1
+port|17095
+type|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
